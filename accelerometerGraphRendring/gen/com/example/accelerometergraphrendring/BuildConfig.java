750772b43fdbaf7bd8da128541c4fd208d8ff5a5
valtph <phvalt@live.fr>
2014-12-18 09:33:16 +0100

    Project Files
    Copy of the Eclipse Folder

---
+++ accelerometerGraphRendring/gen/com/example/accelerometergraphrendring/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.accelerometergraphrendring;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
